3f241ad7e316c7990ddbd89eed14d14fb86c4442
luckily7 <kir.zayczev@gmail.com>
2023-04-19 14:57:03 +0300

    Создана функция public static double distance(Point p1, Point p2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
+
   {
     Point1 p1 = new Point1();
     p1.x = 5;
@@ -10,6 +11,10 @@ public class Point {
     Point2 p2 = new Point2();
     p2.x = 15;
     p2.y = 17;
-
   }
+
+  public static double distance(Point1 p1, Point2 p2) {
+    return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
+  }
+}
 }
