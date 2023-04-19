9a882615718e77f74df0a2da5a48810b45d3a964
luckily7 <kir.zayczev@gmail.com>
2023-04-19 18:51:27 +0300

    Добавлен конструктор this

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,15 +3,12 @@ package ru.stqa.pft.sandbox;
 public class Point {
 
   public static void main(String[] args) {
-    Point1 p1 = new Point1();
-    p1.x = 5;
-    p1.y = 6;
+    Point1 p1 = new Point1(5, 6);
 
-    Point2 p2 = new Point2();
-    p2.x = 15;
-    p2.y = 17;
+    Point2 p2 = new Point2(15, 17);
 
-    System.out.println("Расстояние между точками  " + " = " + distance(p1,p2));
+
+    System.out.println("Расстояние между точками (" + p1.x+";"+p1.y + ") и (" + p2.x+";"+p1.y + ")" + " = " + distance(p1,p2));
   }
 
   public static double distance(Point1 p1, Point2 p2) {
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
@@ -4,4 +4,9 @@ public class Point2 {
 
   public double x;
   public double y;
+
+  public Point2(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 }
