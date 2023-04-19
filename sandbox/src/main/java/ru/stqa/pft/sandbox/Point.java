d7bcddb30b7b9d769412fde578384db7f24422d9
luckily7 <kir.zayczev@gmail.com>
2023-04-19 15:18:17 +0300

    Добавлен запускаемый класс, выводится результат вычисления расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,8 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-
-  {
+  public static void main(String[] args) {
     Point1 p1 = new Point1();
     p1.x = 5;
     p1.y = 6;
@@ -11,10 +10,12 @@ public class Point {
     Point2 p2 = new Point2();
     p2.x = 15;
     p2.y = 17;
+
+    System.out.println("Расстояние между точками  " + " = " + distance(p1,p2));
   }
 
   public static double distance(Point1 p1, Point2 p2) {
     return Math.sqrt((p2.y - p1.y) * (p2.y - p1.y) + (p2.x - p1.x) * (p2.x - p1.x));
   }
 }
-}
+
