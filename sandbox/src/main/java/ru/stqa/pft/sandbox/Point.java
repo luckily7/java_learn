f18fe7e1afc12851d9b9384c26feca3c8ab11ea5
luckily7 <kir.zayczev@gmail.com>
2023-04-20 19:58:19 +0300

    Переименованы классы, удален класс Point2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,14 +2,15 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public static void main(String[] args) {
-    Point1 p1 = new Point1(5, 6);
+  public double x;
+  public double y;
 
-    Point2 p2 = new Point2(15, 17);
-
-
-    System.out.println("Расстояние между точками (" + p1.x+";"+p1.y + ") и (" + p2.x+";"+p2.y + ")" + " = " + p1.distance(p2));
+  public Point(double x, double y) {
+   this.x = x;
+   this.y = y;
   }
 
+  public double distance(Point p2) {
+    return Math.sqrt((p2.y - this.y) * (p2.y - this.y) + (p2.x - this.x) * (p2.x - this.x));
 }
-
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointProgram.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class PointProgram {
+
+  public static void main(String[] args) {
+    Point p1 = new Point(5, 6);
+
+    Point p2 = new Point(15, 17);
+
+
+    System.out.println("Расстояние между точками (" + p1.x+";"+p1.y + ") и (" + p2.x+";"+p2.y + ")" + " = " + p1.distance(p2));
+  }
+
+}
+
