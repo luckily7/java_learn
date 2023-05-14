5051fb9ecf8e739fbc8be947826afd27710053d4
luckily7 <kir.zayczev@gmail.com>
2023-05-14 12:02:43 +0300

    Обновление тестовых данных в PointTests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -14,9 +14,9 @@ public class PointTests {
 
   @Test
   public void testDistance1() {
-    Point p1 = new Point(5, 6);
-    Point p2 = new Point(15, 17);
-    assert p1.distance(p2) == 14.866068747318506;
+    Point p1 = new Point(6, 6);
+    Point p2 = new Point(13, 15);
+    assert p1.distance(p2) == 11.40175425099138;
   }
 }
 
