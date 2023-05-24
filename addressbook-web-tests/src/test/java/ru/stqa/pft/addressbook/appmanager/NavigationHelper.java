f369f02ca21c1e64cbb530aef274246a88a8726e
luckily7 <kir.zayczev@gmail.com>
2023-05-24 14:29:58 +0300

    Revert "Добавлен класс-помощник ContactHelper"
    This reverts commit 033c955728749e306d0c78b0291c9e3fd19a30e1.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,17 +5,11 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-  WebDriver wd;
-
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
     click(By.linkText("groups"));
-  }
-
-  public void gotoHomePage() {
-    wd.findElement(By.linkText("home")).click();
   }
 }
