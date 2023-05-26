0bd07fc9a23e092aa44e7d864212289fc732c3e2
luckily7 <kir.zayczev@gmail.com>
2023-05-26 18:31:18 +0300

    Добавлен тест ContactDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -28,4 +28,16 @@ public class ContactHelper extends HelperBase {
   public void initContactCreation() {
     click(By.linkText("add new"));
   }
+
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+
+  public void deleteSelectedContacts() {
+    click(By.xpath("//input[@value='Delete']"));
+  }
+
+  public void acceptContactsDeletion() {
+    wd.switchTo().alert().accept();
+  }
 }
