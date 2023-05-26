92272303f99459105a3547a62f4082a298f36439
luckily7 <kir.zayczev@gmail.com>
2023-05-26 18:47:33 +0300

    Добавлен тест testContactModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -40,4 +40,12 @@ public class ContactHelper extends HelperBase {
   public void acceptContactsDeletion() {
     wd.switchTo().alert().accept();
   }
+
+  public void initContactModification() {
+    click(By.xpath("//img[@alt='Edit']"));
+  }
+
+  public void submitContactModification() {
+    click(By.name("update"));
+  }
 }
