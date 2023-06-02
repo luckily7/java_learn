d7a1b51a0bd95330aec4ea194cc51a04d431071c
luckily7 <kir.zayczev@gmail.com>
2023-06-02 15:34:52 +0300

    Добавлена возможность запускать тесты в браузерах - IE, СHROME, FIREFOX

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,11 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.Browser;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private final String browser;
   WebDriver wd;
 
   private ContactHelper contactHelper;
@@ -13,8 +18,18 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == Browser.FIREFOX.browserName()) {
+      wd = new FirefoxDriver();
+    } else if (browser == Browser.CHROME.browserName()) {
+      wd = new ChromeDriver();
+    } else if (browser == Browser.IE.browserName()) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     contactHelper = new ContactHelper(wd);
