923504af3924d4ba13ad41eb8de866abe7d0f2c5
luckily7 <kir.zayczev@gmail.com>
2023-09-05 19:55:42 +0300

    Задание №12-13

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -20,6 +22,9 @@ public class ContactDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     ContactDataGenerator generator = new ContactDataGenerator();
     JCommander jCommander = new JCommander(generator);
@@ -34,13 +39,31 @@ public class ContactDataGenerator {
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
-    save(contacts, new File(file));
+    if (format.equals("scv")) {
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void saveAsXml(List<ContactData> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    xstream.allowTypes(new Class[]{ContactData.class});
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress()));
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(),
+              contact.getLastname(), contact.getAddress(), contact.getHome(), contact.getMobile(),
+              contact.getWork(), contact.getEmail(), contact.getEmail2(), contact.getEmail3()));
     }
     writer.close();
   }
@@ -49,7 +72,10 @@ public class ContactDataGenerator {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withFirstname(String.format("Foma %s", i))
-              .withLastname(String.format("Step %s", i)).withAddress(String.format("msk %s", i)));
+              .withLastname(String.format("Kruglov %s", i)).withAddress(String.format("ul. Pechuchkina 56 %s", i))
+              .withHome(String.format("8495756123")).withMobile(String.format("+79265784212"))
+              .withWork(String.format("89265784212")).withEmail(String.format("kruglovray@gmail.com"))
+              .withEmail2(String.format("krug@mail.ru")).withEmail3(String.format("krug@ya.ru")));
     }
     return contacts;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 
-import java.io.File;
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -23,19 +27,30 @@ public class ContactCreationTests extends TestBase {
     app.goTo().homePage();
   }
 
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    BufferedReader reader = new BufferedReader( new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    xstream.allowTypes(new Class[]{ContactData.class});
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/stru.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("Ray").withLastname("Kruglov").withAddress("ul. Pechuchkina 56")
-            .withHome( "84957561234").withMobile("89265784212").withEmail("kruglovray@gmail.com")
-            .withWork("9123434").withEmail2("krug@mail.ru").withEmail3("krug@ya.ru").withPhoto(photo);
     app.contact().create(contact);
     app.goTo().homePage();
-    assertThat(app.contact().count(), equalTo(before.size() + 1));
+    assertThat(app.contact().count(),equalTo(before.size() + 1));
     Contacts after = app.contact().all();
 
     assertThat(after, equalTo(
@@ -52,7 +67,7 @@ public class ContactCreationTests extends TestBase {
 
   }
 
-  @Test
+  @Test (enabled = false)
   public void testBadContactCreation() throws Exception {
     Contacts before = app.contact().all();
     ContactData contact = new ContactData()
