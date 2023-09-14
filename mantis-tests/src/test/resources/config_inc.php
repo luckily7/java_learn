4d8341a9662ad0130bcf59701cf95138fe691e54
luckily7 <kir.zayczev@gmail.com>
2023-09-14 20:29:01 +0300

    Исправлен конфиг config_inc.php

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,9 +1,10 @@
 <?php
 $g_hostname = 'localhost';
-$g_db_type = 'mysql';
+$g_db_type = 'mysqli';
 $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
+$g_crypto_master_salt = 'LP7eBp3eSDyhl6qSMlZxxXJNctSUh0SgP0uWvDeJMEI=';
